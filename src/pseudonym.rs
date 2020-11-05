a473b5b2a93fc79f2a9d5f18e51111d62d5ed78d
Dan Gohman <dev@sunfishcode.online>
2020-11-05 10:00:08 -0800

    Fix a typo in a comment.

--- src/pseudonym.rs
+++ src/pseudonym.rs
@@ -1,6 +1,6 @@
 /// This struct encapsulates the name of an entity whose name is being
 /// hidden in the `nameless` API. It can be written to an `OutputByteStream`
-/// but it otherwise entirely opaque.
+/// but it's otherwise entirely opaque.
 pub struct Pseudonym {
     pub(crate) name: String,
 }
